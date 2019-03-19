8eeb058eaf2c8184cf057302310a446671b47804
marszm <mariusz.szmer@gmail.com>
2019-03-19 16:23:38 +0100

    simple write data to json file

--- src/main/java/com/addressbook/ab/AbApplication.java
+++ src/main/java/com/addressbook/ab/AbApplication.java
@@ -1,13 +1,34 @@
 package com.addressbook.ab;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class AbApplication {
 
     public static void main(String[] args) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = createUserList();
+
+        try {
+            objectMapper.writeValue(new File("C:\\user.json"), user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(AbApplication.class, args);
     }
-
+    private static User createUserList(){
+        User user1 = new User();
+        user1.setName("Mariusz");
+        user1.setSurname("Szmer");
+        User user2 = new User();
+        user2.setName("Andrzej");
+        user2.setSurname("Kolonk");
+        return user2;
+    }
 }
--- src/main/java/com/addressbook/ab/User.java
+++ src/main/java/com/addressbook/ab/User.java
@@ -47,4 +47,6 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+
 }
